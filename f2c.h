a1a7eb3c8f79d39681d43567d20fd203bd008318
Jan Starý <hans@stare.cz>
2024-06-09 08:41:17 +0200

    remove pointless undefs

--- f2c.h
+++ f2c.h
@@ -19,26 +19,4 @@
 #define dmin(a,b) (double)min(a,b)
 #define dmax(a,b) (double)max(a,b)
 
-/* undef any lower-case symbols that your C compiler predefines, e.g.: */
-
-#ifndef Skip_f2c_Undefs
-#undef cray
-#undef gcos
-#undef mc68010
-#undef mc68020
-#undef mips
-#undef pdp11
-#undef sgi
-#undef sparc
-#undef sun
-#undef sun2
-#undef sun3
-#undef sun4
-#undef u370
-#undef u3b
-#undef u3b2
-#undef u3b5
-#undef unix
-#undef vax
-#endif
 #endif
